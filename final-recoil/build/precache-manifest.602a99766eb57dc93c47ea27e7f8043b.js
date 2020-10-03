02593dd574f4b487d71bceb7ae4cbcbc774cb685
askharley <harl3y.fer@gmail.com>
2020-10-03 23:49:16 +0200

    Added react-scripts to root package.

---
+++ final-recoil/build/precache-manifest.602a99766eb57dc93c47ea27e7f8043b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7165a72792a8ce4cc0272256f02e484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d749531f2072281ae01",
+    "url": "/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "3e4afc1243e3d028891f",
+    "url": "/static/css/main.248c66e0.chunk.css"
+  },
+  {
+    "revision": "1d749531f2072281ae01",
+    "url": "/static/js/2.119e2a5c.chunk.js"
+  },
+  {
+    "revision": "3e4afc1243e3d028891f",
+    "url": "/static/js/main.bb7bef92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
